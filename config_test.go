db1153431b1313b0da5e8f697d0420776deefcf6
alessio <asoseil@gmail.com>
2017-04-12 09:22:01 +0200

    test read fixed

--- config_test.go
+++ config_test.go
@@ -1,45 +1,50 @@
 package fresh
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"os"
-	"testing"
-	"encoding/json"
 	"path/filepath"
+	"testing"
 )
 
-func temp() (d string, err error){
-	d, err = ioutil.TempDir(os.TempDir(),dir)
+func temp() (d string, err error) {
+	d, err = ioutil.TempDir(os.TempDir(), dir)
 	return
 }
 
 func TestRead(t *testing.T) {
-	config := &config{}
-	content, err := json.Marshal(config)
-	if err != nil{
-		t.Error(err)
-	}
-	path, err := temp()
-	if err != nil{
-		t.Error(err)
-	}
-	ioutil.WriteFile(filepath.Join(path,file), content, perm)
+	result := config{port: 20}
+	expected := config{port: 20}
+
+	content, err := json.Marshal(&expected)
 	if err != nil {
 		t.Error(err)
 	}
-	if err = config.read(path); err != nil{
+	path, err := temp()
+	if err != nil {
 		t.Error(err)
+	}
+	ioutil.WriteFile(filepath.Join(path, file), content, perm)
+	if err != nil {
+		t.Error(err)
+	}
+	if err = result.read(path); err != nil {
+		t.Error(err)
+	}
+	if result != expected {
+		t.Error("Expected:", expected, "instead", result)
 	}
 	os.Remove(path)
 }
 
-func TestWrite(t *testing.T){
+func TestWrite(t *testing.T) {
 	config := &config{}
 	path, err := temp()
-	if err != nil{
+	if err != nil {
 		t.Error(err)
 	}
-	if err = config.write(path); err != nil{
+	if err = config.write(path); err != nil {
 		t.Error(err)
 	}
 }
