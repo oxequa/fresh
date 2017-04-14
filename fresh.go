c166be099f3d8319b0077dbea9ad495a4c2334e4
alessio <asoseil@gmail.com>
2017-04-15 00:38:57 +0200

    gracefull shutdown

--- fresh.go
+++ fresh.go
@@ -1,10 +1,14 @@
 package fresh
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"context"
+	"time"
 )
 
 type (
@@ -33,13 +37,21 @@ func New(h string, p string) Fresh {
 }
 
 func (f *fresh) Run() {
+	shutdown := make(chan os.Signal)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
 	listener, err := net.Listen("tcp", f.host+":"+f.port)
 	if err != nil {
 		os.Exit(1)
 	}
-	fmt.Println("Server started on " + f.host + ":" + f.port)
-	f.service.server.Handler = f.service.handler
-	f.service.server.Serve(listener)
+	go func() {
+		log.Println("Server started on " + f.host + ":" + f.port)
+		f.service.server.Handler = f.service.handler
+		f.service.server.Serve(listener)
+	}()
+	<-shutdown
+	log.Println("Shutting down server...")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	f.service.server.Shutdown(ctx)
 }
 
 func (f *fresh) Get(p string, h func()) {
