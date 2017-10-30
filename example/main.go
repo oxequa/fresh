298bfe0c1e7622a0fd2ce60744b79668b200cd66
asoseil <asoseil@gmail.com>
2017-10-30 14:51:03 +0100

    websocket example

--- example/main.go
+++ example/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-"github.com/tockins/fresh"
-"net/http"
-"github.com/tockins/fresh/example/models"
+	"github.com/tockins/fresh"
+	"net/http"
+	"github.com/tockins/fresh/example/models"
+	"golang.org/x/net/websocket"
+	"fmt"
 )
-
-
 
 func main() {
 	f := fresh.New()
@@ -16,6 +16,7 @@ func main() {
 	g.GET("/", list)
 	g.GET("/{todoUuid}", single)
 	g.GET("/{todoUuid}/users/{userUuid}", single)
+	f.WS("ws",socket)
 	f.Run()
 }
 
@@ -37,3 +38,23 @@ func filter(f fresh.Context) error{
 	return nil
 }
 
+
+func socket(f fresh.Context) error{
+	for {
+		ws := f.Request().WS()
+		// Write
+		err := websocket.Message.Send(ws, "Hello, Client!")
+		if err != nil {
+			fmt.Println("E",err)
+		}
+		//
+		//// Read
+		msg := ""
+		err = websocket.Message.Receive(ws, &msg)
+		if err != nil {
+			fmt.Println("E1",err)
+		}
+		fmt.Println("msg", msg)
+	}
+	return nil
+}
