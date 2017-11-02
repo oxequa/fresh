be96ac12f08a4bab150ba81d921ffa3809e2db2c
asoseil <asoseil@gmail.com>
2017-11-02 19:56:27 +0100

    get test

--- fresh_test.go
+++ fresh_test.go
@@ -1,28 +1,107 @@
 package fresh
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
-	"strconv"
-	"strings"
 	"testing"
-	"math/rand"
+	"net/http/httptest"
+	"io"
+	"encoding/json"
 )
 
-var PORT = rand.Intn(9999-1111) + 1111
-
-type Data struct {
-	Data string `json:"data"`
+type r struct {
+	path string
+	args []string
 }
 
-var REQUESTS = map[string][]string{
-	"/first/":                    {""},
-	"/first/:/":                  {"1"},
-	"/first/:/second/":           {"1"},
-	"/first/:/second/:/":         {"1", "2"},
-	"/first/:/second/:/third/":   {"1", "2"},
-	"/first/:/second/:/third/:/": {"1", "2", "3"},
+var routes = []r{
+	{
+		path: "/first/",
+		args: []string{},
+	},
+	{
+		path: "/first/:/",
+		args: []string{"1"},
+	},
+	{
+		path: "/first/:/second",
+		args: []string{"1"},
+	},
+	{
+		path: "/first/:/second/:/",
+		args: []string{"1","2"},
+	},
+	{
+		path: "/first/:/second/:/third",
+		args: []string{"1","2"},
+	},
+	{
+		path: "/first/:/second/:/third/:/",
+		args: []string{"1","2","3"},
+	},
+}
+
+func ctrl(args []string) HandlerFunc{
+	return func(context Context) error {
+		data := make(map[string]string)
+		for _, val := range args {
+			data[val] = val
+		}
+		return context.Response().JSON(http.StatusOK, data)
+	}
+}
+
+func setup() (fresh){
+	f := fresh{
+		config: new(config),
+		server: new(http.Server),
+		router: &router{&route{}, &context{}},
+	}
+	return f
+}
+
+func requests(method string, f *fresh){
+	for _, elm := range routes {
+		switch method {
+		case "GET":
+			f.GET(elm.path,ctrl(elm.args))
+		case "POST":
+			f.POST(elm.path,ctrl(elm.args))
+		case "PUT":
+			f.PUT(elm.path,ctrl(elm.args))
+		case "TRACE":
+			f.TRACE(elm.path,ctrl(elm.args))
+		case "PATCH":
+			f.PATCH(elm.path,ctrl(elm.args))
+		case "DELETE":
+			f.DELETE(elm.path,ctrl(elm.args))
+		case "OPTIONS":
+			f.OPTIONS(elm.path,ctrl(elm.args))
+		}
+	}
+}
+
+func records(method string, body io.Reader, f fresh, t *testing.T){
+	for _, elm := range routes {
+		rec := httptest.NewRecorder()
+		req, err := http.NewRequest(method, elm.path, body)
+		if err != nil {
+			t.Fatal("Creating", method, elm, "request failed!")
+		}
+		f.router.ServeHTTP(rec,req)
+		if rec.Code != http.StatusOK {
+			t.Fatal("Server error: Returned ", rec.Code, " instead of ", http.StatusOK)
+		}
+		data := make(map[string]string)
+		err = json.NewDecoder(rec.Body).Decode(&data)
+		if err != nil {
+			t.Error(err)
+		}
+		for _, value := range elm.args {
+			if data[value] != value {
+				t.Error("Response mismatch")
+			}
+		}
+	}
 }
 
 func TestFresh_Run(t *testing.T) {
@@ -40,91 +119,7 @@ func TestFresh_Run(t *testing.T) {
 }
 
 func TestFresh_GET(t *testing.T) {
-	f := New()
-	f.Config().SetPort(PORT)
-	for key, values := range REQUESTS {
-		data := make(map[string]string)
-		for _, val := range values {
-			data[val] = val
-		}
-		f.GET(makeRequestURL(key, values, true), func(c Context) error {
-			return c.Response().JSON(http.StatusOK, data)
-		})
-	}
-	go func() {
-		f.Run()
-	}()
-	for key, values := range REQUESTS {
-		resp, err := http.Get(makeRequestURL(key, values, false))
-		if err != nil {
-			t.Error(err)
-		}
-		if resp.StatusCode != 200 {
-			t.Error(resp.StatusCode)
-		}
-		data := make(map[string]string)
-		err = json.NewDecoder(resp.Body).Decode(&data)
-		if err != nil {
-			t.Error(err)
-		}
-		for _, value := range values {
-			if data[value] != value {
-				t.Error("Response mismatch")
-			}
-		}
-	}
-	f.Shutdown()
-}
-
-func TestFresh_POST(t *testing.T) {
-	f := New()
-	f.Config().SetPort(PORT)
-	for key, values := range REQUESTS {
-		data := make(map[string]string)
-		for _, val := range values {
-			data[val] = val
-		}
-		f.POST(makeRequestURL(key, values, true), func(c Context) error {
-			data["body"] = c.Request().FormValue("body")
-			return c.Response().JSON(http.StatusOK, data)
-		})
-	}
-	go func() {
-		f.Run()
-	}()
-	for key, values := range REQUESTS {
-		body := map[string]string{"body": "body"}
-		jsonBody, _ := json.Marshal(body)
-		resp, err := http.Post(makeRequestURL(key, values, false), "application/json", bytes.NewBuffer(jsonBody))
-		if err != nil {
-			t.Error(err)
-		}
-		if resp.StatusCode != 200 {
-			t.Error(resp.StatusCode)
-		}
-		data := make(map[string]string)
-		err = json.NewDecoder(resp.Body).Decode(&data)
-		if err != nil {
-			t.Error(err)
-		}
-		for _, value := range values {
-			if data[value] != value {
-				t.Error("Response mismatch")
-			}
-		}
-	}
-	f.Shutdown()
-}
-
-func makeRequestURL(url string, values []string, parameter bool) string {
-	sep := ""
-	host := "http://127.0.0.1:" + strconv.Itoa(PORT)
-	if parameter {
-		sep = ":"
-		host = ""
-	}
-	for _, value := range values {
-		url = strings.Replace(url, ":/", sep+value+"/", 1)
-	}
-	return host + url
+	f := setup()
+	requests("GET", &f)
+	records("GET",nil, f, t)
 }
